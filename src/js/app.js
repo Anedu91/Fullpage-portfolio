ae588b2e1746cde83d7f51fc3c58911297ad9dc1
Angel <diaz.angeledu@gmail.com>
2021-01-26 15:56:21 -0400

    Fixing final details

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 /* FULLPAGE SETTINGS */
 const navItem = document.querySelectorAll(".nav__item");
-const contactButton = document.querySelector("#contact")
+const contactButton = document.querySelector("#contact");
 const FullPage = new fullpage("#fullpage", {
   css3: true,
   anchors: ["app"],
@@ -9,22 +9,24 @@ const FullPage = new fullpage("#fullpage", {
   fixedElements: ".header, .nav, #circle",
 
   onSlideLeave: function (section, origin, destination, direction) {
-    const nextContainer = document.getElementsByClassName(destination.anchor)[0].getElementsByClassName('container')[0];
-    nextContainer.classList.remove('animation-right');
-    nextContainer.classList.remove('animation-left'); 
-    
-    window.requestAnimationFrame(() => {
-      direction === 'right' ? nextContainer.classList.add('animation-right') : nextContainer.classList.add('animation-left')      
-    })   
-    
-  },
-  afterSlideLoad: function(anchorLink, index, sliderAnchor, slideIndex) {    
+    const nextContainer = document
+      .getElementsByClassName(destination.anchor)[0]
+      .getElementsByClassName("container")[0];
+    nextContainer.classList.remove("animation-right");
+    nextContainer.classList.remove("animation-left");
 
-    navItem.forEach(navLink => {
-      navLink.classList.remove('active')
-    })
-    navItem[sliderAnchor.index].classList.add('active')
-  }
+    window.requestAnimationFrame(() => {
+      direction === "right"
+        ? nextContainer.classList.add("animation-right")
+        : nextContainer.classList.add("animation-left");
+    });
+  },
+  afterSlideLoad: function (anchorLink, index, sliderAnchor, slideIndex) {
+    navItem.forEach((navLink) => {
+      navLink.classList.remove("active");
+    });
+    navItem[sliderAnchor.index].classList.add("active");
+  },
 });
 
 navItem.forEach((item, index) => {
@@ -32,9 +34,9 @@ navItem.forEach((item, index) => {
     fullpage_api.moveTo("app", index);
   });
 });
-contactButton.addEventListener('click', () => {
-  fullpage_api.moveTo('app',3)
-})
+contactButton.addEventListener("click", () => {
+  fullpage_api.moveTo("app", 3);
+});
 
 /* SKILLS PROGRESS SETTINGS */
 
@@ -52,40 +54,38 @@ const changingNumber = (percent) => {
   number.innerText = percent;
 };
 const gettingActive = (button) => {
-  progressButtons.forEach(navItem => {
-    navItem.classList.remove('active')
-    button.classList.add('active')
-  })
+  progressButtons.forEach((navItem) => {
+    navItem.classList.remove("active");
+    button.classList.add("active");
+  });
 };
 const gettingProgress = (button) => {
   const progress = button.dataset.progress;
   showingProgress(progress);
   changingNumber(progress);
-  gettingActive(button)
+  gettingActive(button);
 };
 
 /* Cursor follow */
 const circle = document.querySelector("#circle");
 
 const onMouseMove = (e) => {
-  circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';
-}
+  circle.style.left = e.pageX + "px";
+  circle.style.top = e.pageY + "px";
+};
 
-
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener("DOMContentLoaded", function () {
   gettingProgress(actualButton);
 
-  document.addEventListener('mousemove', onMouseMove)
+  document.addEventListener("mousemove", onMouseMove);
 
   progressButtons.forEach((button) => {
     button.addEventListener("click", () => gettingProgress(button));
   });
-  document.addEventListener('click', () => {
-    circle.classList.remove('animation');
+  document.addEventListener("click", () => {
+    circle.classList.remove("animation");
     window.requestAnimationFrame(() => {
-      circle.classList.add('animation');
-    })
-  })
-  
-})
+      circle.classList.add("animation");
+    });
+  });
+});
